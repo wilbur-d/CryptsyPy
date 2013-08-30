e606d1e3e5543272c7b745f2f74f6d76857b17bb
geod <geod@kowal>
2013-08-30 09:58:46 -0400

    1st Update from Eclipse

--- crypt.py
+++ crypt.py
@@ -2,22 +2,27 @@
 
 import Cryptsy
 import ciphile
-import sys
 import os
 import getpass
-from hashlib import md5, sha512
+from hashlib import sha512
+import ConfigParser
 
 retries = 0
 pw = getpass.getpass('Password: ')
 print sha512(pw).hexdigest() 
 while retries < 3:
-	if sha512(pw).hexdigest() == '70bd6161ae1cdbd61857e2009e19d0e620452004037394c5340fdb3f158ae958384efeaa407e9f41df00191edec8b01ff675f1420d2194eb75fa4307e3c6ed3f':
-		with open('crypt.conf', 'rb') as in_file, open('crypt.enc', 'wb') as out_file:
-			ciphile.encrypt(in_file, out_file, pw)
-			break
-	else:
-		pw = getpass.getpass('Incorrect Password, try again...')
-		retries += 1
+    if sha512(pw).hexdigest() == '70bd6161ae1cdbd61857e2009e19d0e620452004037394c5340fdb3f158ae958384efeaa407e9f41df00191edec8b01ff675f1420d2194eb75fa4307e3c6ed3f':
+        with open('crypt.conf', 'rb') as in_file, open('crypt.enc', 'wb') as out_file:
+            ciphile.decrypt(in_file, out_file, pw)
+            in_file.close()
+            out_file.close()
+            Config = ConfigParser.ConfigParser()
+            Config.read('./')
+
+            break
+    else:
+        pw = getpass.getpass('Incorrect Password, try again...')
+        retries += 1
 
 
 del pw
