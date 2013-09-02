748ee746aa778f93186080ca8e47a7baf26fc0cc
The Geo Daemon <geod@ubuvm>
2013-09-02 04:22:47 -0400

    Crypto Coming Along: Able to encrypt single var

--- ciphvar.py
+++ ciphvar.py
@@ -10,31 +10,28 @@ def derive_key_and_iv(password, salt, key_length, iv_length):
         d += d_i
     return d[:key_length], d[key_length:key_length+iv_length]
 
-def encrypt(in_file, out_file, password, key_length=32):
+def encrypt(invar, password, key_length=32):
     bs = AES.block_size
-    salt = Random.new().read(bs - len('Salted__'))
+    salt = Random.new().read(bs)
+    print salt
     key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    out_file.write('Salted__' + salt)
-    finished = False
-    while not finished:
-        chunk = in_file.read(1024 * bs)
-        if len(chunk) == 0 or len(chunk) % bs != 0:
-            padding_length = (bs - len(chunk) % bs) or bs
-            chunk += padding_length * chr(padding_length)
-            finished = True
-        out_file.write(cipher.encrypt(chunk))
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    return salt + cipher.encrypt(invar)
 
-def decrypt(in_file, out_file, password, key_length=32):
+def decrypt(invar, password, key_length=32):
     bs = AES.block_size
-    salt = in_file.read(bs)[len('Salted__'):]
+    salt = invar[:bs]
+    #print salt
     key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    next_chunk = ''
-    finished = False
-    while not finished:
-        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
-        if len(next_chunk) == 0:
-            chunk = chunk.rstrip(chunk[-1])
-            finished = True
-        out_file.write(chunk)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    return cipher.decrypt(invar[16:])
+
+"""
+msg = 'Tootsy rollzaaaaaaPOLZANYUKUH TOOGE12312938a;sldkfj'
+msg = encrypt(msg, 'toot')
+
+print msg
+
+msg = decrypt(msg, 'toot')
+
+print msg"""
--- crypt.py
+++ crypt.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python
 
 import Cryptsy
-import ciphile
+from ciphvar import *
 import os
 import getpass
 from hashlib import sha512
 import ConfigParser
 
+Config = ConfigParser.ConfigParser()
+Config.read('./crypt.conf')
 retries = 0
 pw = getpass.getpass('Password: ')
-print sha512(pw).hexdigest() 
-while retries < 3:
-    if sha512(pw).hexdigest() == '70bd6161ae1cdbd61857e2009e19d0e620452004037394c5340fdb3f158ae958384efeaa407e9f41df00191edec8b01ff675f1420d2194eb75fa4307e3c6ed3f':
-        with open('crypt.conf', 'rb') as in_file, open('crypt.enc', 'wb') as out_file:
-            ciphile.decrypt(in_file, out_file, pw)
-            in_file.close()
-            out_file.close()
-            Config = ConfigParser.ConfigParser()
-            Config.read('./')
 
+pub = Config.get('Api-Key', 'pub')
+key = Config.get('Api-Key', 'key').decode('string_escape')
+
+#print sha512(pw).hexdigest() 
+
+while retries < 3:
+    # @TODO: Pull hash from Database
+    if sha512(pw).hexdigest() == '70bd6161ae1cdbd61857e2009e19d0e620452004037394c5340fdb3f158ae958384efeaa407e9f41df00191edec8b01ff675f1420d2194eb75fa4307e3c6ed3f':
+            key = decrypt(key, pw)
+            print key
             break
     else:
         pw = getpass.getpass('Incorrect Password, try again...')
         retries += 1
-
-
 del pw
 
-os.rename('crypt.enc', 'crypt.conf')
+#os.rename('crypt.enc', 'crypt.conf')
+#print 'Key:\t:' + key
+#print 'Priv:\t:' + key
 
-marketd = Cryptsy.Cryptsy(key, secret)
+marketd = Cryptsy.Cryptsy(pub, key)
 datum = marketd.getMarkets()
-print datum['return'][0]['high_trade']
+print datum
 for x in datum['return']:
     print "%s Vol:  %s" %(x['primary_currency_code'], x['current_volume'])
