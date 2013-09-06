8f8188428c348fbc5e63172bed358c6275b1d04a
geojitsu <brokincognito@gmail.com>
2013-09-06 06:18:46 -0400

    DB Update from Dons

--- crypt.py
+++ crypt.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python
 
-import Cryptsy
-from ciphvar import *
-import os
-import getpass
 from hashlib import sha512
 from passlib import hash
+from ciphvar import *
 import ConfigParser
+import getpass
 import sqlite3
+import Cryptsy
 
-db = 'cobot.db'
+global db = 'cobot.db'
+
 def getCreds(dbname, uname):
     conn = sqlite3.connect(dbname)
     cont = conn.cursor()
     cont.execute('SELECT hash FROM users WHERE user=?', uname)
-    return cont.fetchone()[0].split('$')[3]
-
-hashCheck = getCreds(db, ('geod',))
-print hashCheck
-
+    hashSplice = cont.fetchone()[0]
+    return (hashSplice.split('$')[2], hashSplice.split('$')[3])
 
 
 Config = ConfigParser.ConfigParser()
 Config.read('./crypt.conf')
-retries = 0
+
+cryptsyPub = Config.get('Api-Key', 'pub')
+cryptsyKey = Config.get('Api-Key', 'key').decode('string_escape')   # decode() is necesary to strip
+                                                                    # extra escape chars
+
+
+hashsalt, hashdb = getCreds(db, ('geod',))
+
 pw = getpass.getpass('Password: ')
 
-pub = Config.get('Api-Key', 'pub')
-key = Config.get('Api-Key', 'key').decode('string_escape') # decode() is necesary to strip
-														   # extra escape chars
+hashCheck = hash.sha512_crypt.encrypt(pw, salt=hashsalt, rounds=5000).split('$')[3]
 
-#print sha512(pw).hexdigest() 
-
+retries = 0
 while retries < 3:
-    # @TODO: Pull hash from Database
-    if hash.sha512_crypt.encrypt(pw, salt='HX', rounds=5000) == hashCheck:
-            key = decrypt(key, pw)
-            print key
+    if hashCheck == hashdb:
+            cryptsyKey = decrypt(cryptsyKey, pw)
+            print cryptsyKey
             break
     else:
         pw = getpass.getpass('Incorrect Password, try again...')
         retries += 1
 del pw
 
-#os.rename('crypt.enc', 'crypt.conf')
-#print 'Key:\t:' + key
-#print 'Priv:\t:' + key
-
-marketd = Cryptsy.Cryptsy(pub, key)
+marketd = Cryptsy.Cryptsy(cryptsyPub, cryptsyKey)
 datum = marketd.getMarkets()
 print datum
 for x in datum['return']:
